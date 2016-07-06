919c180e4188002305098646fdc9ea7e83134e9b
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-07-05 23:12:23 -0700

    lint utils

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,15 @@
 // credit to Mapbox
 // via: https://github.com/mapbox/to-color
 export function toColor(str, opacity) {
-  var rgb = [0, 0, 0, opacity || 0.75];
+  const rgb = [0, 0, 0, opacity || 0.75];
   try {
-    for (var i = 0; i < str.length; i++) {
-      var v = str.charCodeAt(i);
-      var idx = v % 3;
+    for (let i = 0; i < str.length; i++) {
+      const v = str.charCodeAt(i);
+      const idx = v % 3;
       rgb[idx] = (rgb[i % 3] + (13 * (v % 13))) % 20;
     }
   } finally {
-  return 'rgba(' +
-    rgb.map(function(c, idx) { // eslint-disable-line no-shadow
-      return idx === 3 ? c : (4 + c) * 17;
-    }).join(',') + ')';
+    return `rgba(${rgb.map((c, idx) => idx === 3 ? c : (4 + c) * 17).join(',')})`;
   }
 }
 
@@ -20,10 +17,9 @@ export function oclock(time) {
   if (time === 0) {
     return '12am';
   } else if (time < 12) {
-    return time + 'am';
+    return `${time}am`;
   } else if (time === 12) {
-    return time + 'pm';
-  } else {
-    return (time % 12) + 'pm';
+    return `${time}pm`;
   }
+  return `${(time % 12)}pm`;
 }

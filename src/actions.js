fae894d3323edc8cf32f9c060cefb15dc6bf56a2
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-07-06 23:03:00 -0700

    add removeSection

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,7 @@
 export const ADD_COURSE = 'ADD_COURSE';
 export const REMOVE_COURSE = 'REMOVE_COURSE';
 export const ADD_SECTION = 'ADD_SECTION';
+export const REMOVE_SECTION = 'REMOVE_SECTION';
 export const TOGGLE_SECTION = 'TOGGLE_SECTION';
 export const SELECT_COURSE = 'SELECT_COURSE';
 
@@ -14,6 +15,10 @@ export function removeCourse(dept, level) {
 
 export function addSection(dept, level, section) {
   return { type: ADD_SECTION, dept, level, section };
+}
+
+export function removeSection(dept, level, section) {
+  return { type: REMOVE_SECTION, dept, level, section };
 }
 
 export function toggleSection(dept, level, section) {

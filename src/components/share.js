ce081364f2c9b95f5a2bbb3d52a57b32d33a99bc
kelvinabrokwa <kelvinabrokwa@gmail.com>
2017-01-03 01:52:23 -0500

    select share url on click

--- src/components/share.js
+++ src/components/share.js
@@ -1,19 +1,33 @@
-const Share = ({ queryString }) => (<div className='pad2'>
-  <div>
-    <div>
-      share your sched with the homies:
-    </div>
-    <div>
-      <input
-        type='text'
-        className=''
-        style={{ width: '100%' }}
-        value={`http://abrokwa.org/sched/?${queryString}`}
-        readOnly={true}
-      />
-    </div>
-  </div>
-</div>);
+/**
+ * Share Component
+ */
+class Share extends React.Component {
+
+  onClick(e) {
+    e.target.setSelectionRange(0, e.target.value.length)
+  }
+
+  render() {
+    return (<div className='pad2'>
+      <div>
+        <div>
+          share your sched with the homies:
+        </div>
+        <div>
+          <input
+            type='text'
+            className=''
+            style={{ width: '100%' }}
+            value={`http://abrokwa.org/sched/?${this.props.queryString}`}
+            readOnly={true}
+            onClick={this.onClick.bind(this)}
+          />
+        </div>
+      </div>
+    </div>);
+  }
+
+}
 
 /*
 Share.propTypes = {

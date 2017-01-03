413da25210a0eead2cf6b3f9bc5d832afafc0ba6
kelvinabrokwa <kelvinabrokwa@gmail.com>
2017-01-03 03:45:49 -0500

    use semester name for geocoding

--- map_worker.js
+++ map_worker.js
@@ -6,7 +6,7 @@ onmessage = e => {
   const { data } = e;
 
   if (self.fetch) {
-    fetch(`https://wm-course-data.herokuapp.com/geocode/${201720}/${data.crn}`)
+    fetch(`https://wm-course-data.herokuapp.com/geocode/${data.semester}/${data.crn}`)
       .then(res => res.json())
         .then(d => {
           if (!d.err) {
@@ -20,7 +20,7 @@ onmessage = e => {
       if (!d.err)
         postMessage(Object.assign(d, data));
     });
-    xhr.open('GET', `https://wm-course-data.herokuapp.com/geocode/${201720}/${data.crn}`);
+    xhr.open('GET', `https://wm-course-data.herokuapp.com/geocode/${data.semester}/${data.crn}`);
     xhr.send();
   }
 }
